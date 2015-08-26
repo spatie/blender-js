919c2a0a3af4bac2e3cef57d7a51256a5d95bfa0
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2015-08-26 17:02:38 +0200

    Datepicker format

--- modules/form.input.datetimepicker.js
+++ modules/form.input.datetimepicker.js
@@ -1,13 +1,13 @@
 // Remember form draft
 
 var $ = require('jquery');
-var ß = require("./../blender.js");
+var ß = require('./../blender.js');
 require('jquery-datetimepicker');
 
-$("[data-datetimepicker]").datetimepicker({
+$('[data-datetimepicker]').datetimepicker({
     timepicker: false,
     lang: ß.lang,
     dayOfWeekStart: 1,
     scrollInput: false,
-    format: "Y/m/d"
+    format: 'd/m/Y'
 });
