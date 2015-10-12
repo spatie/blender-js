8a9064f3bb0587c17e35ec3c782067d4cae1d1e2
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2015-10-12 14:57:38 +0200

    Autosave warning can now be called externally

--- modules/form.autosave.js
+++ modules/form.autosave.js
@@ -1,24 +1,33 @@
 // Autosave form input
-var $ = require('jquery');
-require('sisyphus.js');
-var translate = require("./interface.translations.js");
-
+var $ = require('jquery')
+require('sisyphus.js')
+var translate = require("./interface.translations.js")
 
 $("form[data-autosave]").each(function () {
-    var $form = $(this);
+    var $form = $(this)
 
-    //construct warning
-    var $erase = $('<a href="#">' + translate('sisyphus.revert') + '</a>').on('click', function (e) {
-        e.preventDefault();
-        $form.sisyphus().manuallyReleaseData();
-        window.location.reload();
-    });
-    var $warn = $('<div class="alert -warning">' + translate('sisyphus.warn') + ' </div>').append($erase);
+    var $erase = $('<a href="?revert=1">'+translate('sisyphus.revert')+'</a>')
+        .on('click', function (e) {
+            e.preventDefault()
+            $form.sisyphus().manuallyReleaseData()
+            window.location = $(this).attr('href')
+        })
 
-    //init sisyphus
-    $form.sisyphus({
-        onRestore: function () {
-            $form.before($warn);
+    var $warn = $('<div class="alert -warning" data-autosave-warn>'+translate('sisyphus.warn')+'</div>').append($erase)
+
+    // Add the function to the DOM node so it can be called externally
+    $form[0].addDraftWarning = function() {
+
+        // Prevent duplicate warnings
+        if ($('[data-autosave-warn]').length) {
+            return
         }
-    });
-});
+
+        $form.before($warn)
+    }
+
+    // Init sisyphus
+    $form.sisyphus({
+        onRestore: $form[0].addDraftWarning
+    })
+})
