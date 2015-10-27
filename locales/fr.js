d2ace6e3877cabfc635380badd4f5cc431d733e3
Freek Van der Herten <freek@spatie.be>
2015-10-27 16:30:24 +0100

    Create fr.js

---
+++ locales/fr.js
@@ -0,0 +1,41 @@
+var ß = require("../blender.js");
+
+ß.translations.nl = {
+    "confirm": {
+        "text": "Bent u zeker?",
+        "yes": "Oui",
+        "no": "Non",
+    },
+    "dataTables": {
+        "lengthMenu": "Afficher _MENU_ par page",
+        "zeroRecords": "Aucun résultat",
+        "info": "Nobre: _TOTAL_",
+        "infoEmpty": "Pas d'objects",
+        "infoFiltered": "des _MAX_",
+        "search": "_INPUT_",
+        "searchPlaceholder": "Filtre..."
+    },
+    "sisyphus": {
+        "revert": "Retour à la dernière version enregistrée",
+        "warn": "Cette page contient des données non enregistrées."
+    },
+    "parts": {
+        "added": "toegevoegd",
+        "upload": {
+            "maxNumberOfFiles": "Maximum aantal bestanden overschreden",
+            "acceptFileTypes": "Nombre maximum de fichiers dépassé",
+            "maxFileSize": "Fichier est trop volumineux",
+            "minFileSize": "Fichier est trop petit",
+            "fail": "Quelque chose a mal tourné dans le traitement"
+        }
+    },
+    "locationpicker": {
+        markerText: "Glisser pour déplacer",
+        errorEmpty: "oordonnées pour cette adresse n'a pu être trouvée",
+        errorNoResults: "Coordonnées pour cette adresse n'a pu être trouvé"
+    },
+    "select": {
+        select: "Sélectionner...",
+        noResults: "Aucun résultat"
+    }
+}
